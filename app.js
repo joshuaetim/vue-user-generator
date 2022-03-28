df29bd4e3e5b2ba910f992fd8e1e50ca17a57c12
Joshua Etim <jetimworks@gmail.com>
2022-03-28 23:17:35 +0100

    vue-user-generator

---
+++ app.js
@@ -0,0 +1,36 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: 'John',
+            lastName: 'Doe',
+            gender: 'male',
+            title: "Mr.",
+            picture: 'https://randomuser.me/portraits/men/20.jpg',
+            email: 'patrickstar@gmail.com',
+            dob: 'ttt',
+            age: 23,
+            street: 'rand',
+            postcode: 'rand',
+            state: 'rand',
+            city: 'rand',
+            phone: '444'
+
+        }
+    },
+    methods: {
+        async getUser(){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            // console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    },
+})
+
+app.mount('#app')
