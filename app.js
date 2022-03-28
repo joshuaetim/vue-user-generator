62078c6c3fad3d1ecfca098b558350d50d40f1d3
Joshua Etim <jetimworks@gmail.com>
2022-03-29 00:44:53 +0100

    extra fields, fetch

--- app.js
+++ app.js
@@ -13,9 +13,45 @@ const app = Vue.createApp({
             postcode: 'rand',
             state: 'rand',
             city: 'rand',
-            phone: '444'
+            phone: '444',
+            username: 'josh',
+            password: 'secret'
 
         }
+    },
+    created() {
+        fetch('https://randomuser.me/api')
+        .then(function(res) {
+            return res.json()
+        })
+        .then(result => {
+            let results = result.results
+
+            this.dob = results[0].dob.date
+            this.age = results[0].dob.age
+            this.title = results[0].name.title
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+            this.street = results[0].location.street.number + " " + results[0].location.street.name
+            this.city = results[0].location.city
+            this.state = results[0].location.state
+            this.postcode = results[0].location.postcode
+            this.phone = results[0].phone
+            this.username = results[0].login.username
+            this.password = results[0].login.password
+
+        })
+
+        // console.log(results)
+
+        // this.firstName = results[0].name.first
+        // this.lastName = results[0].name.last
+        // this.email = results[0].email
+        // this.gender = results[0].gender
+        // this.picture = results[0].picture.large
     },
     methods: {
         async getUser(){
