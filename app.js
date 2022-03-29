96642c9c6f28fe0aa8a917231f51f96e6066e5ae
Joshua Etim <jetimworks@gmail.com>
2022-03-29 04:54:10 +0100

    fixed button issue

--- app.js
+++ app.js
@@ -19,54 +19,39 @@ const app = Vue.createApp({
 
         }
     },
-    created() {
-        fetch('https://randomuser.me/api')
-        .then(function(res) {
-            return res.json()
-        })
-        .then(result => {
-            let results = result.results
-
-            this.dob = results[0].dob.date
-            this.age = results[0].dob.age
-            this.title = results[0].name.title
-            this.firstName = results[0].name.first
-            this.lastName = results[0].name.last
-            this.email = results[0].email
-            this.gender = results[0].gender
-            this.picture = results[0].picture.large
-            this.street = results[0].location.street.number + " " + results[0].location.street.name
-            this.city = results[0].location.city
-            this.state = results[0].location.state
-            this.postcode = results[0].location.postcode
-            this.phone = results[0].phone
-            this.username = results[0].login.username
-            this.password = results[0].login.password
-
-        })
-
-        // console.log(results)
-
-        // this.firstName = results[0].name.first
-        // this.lastName = results[0].name.last
-        // this.email = results[0].email
-        // this.gender = results[0].gender
-        // this.picture = results[0].picture.large
+    mounted() {
+        fetchDataAPI(this)
     },
     methods: {
-        async getUser(){
-            const res = await fetch('https://randomuser.me/api')
-            const { results } = await res.json()
-
-            // console.log(results)
-
-            this.firstName = results[0].name.first
-            this.lastName = results[0].name.last
-            this.email = results[0].email
-            this.gender = results[0].gender
-            this.picture = results[0].picture.large
+        getUser(){
+            fetchDataAPI(this)
         }
     },
 })
+
+function updateFields(that, results) {
+    that.dob = new Date(results[0].dob.date).toLocaleDateString()
+    that.age = results[0].dob.age
+    that.title = results[0].name.title
+    that.firstName = results[0].name.first
+    that.lastName = results[0].name.last
+    that.email = results[0].email
+    that.gender = results[0].gender
+    that.picture = results[0].picture.large
+    that.street = results[0].location.street.number + " " + results[0].location.street.name
+    that.city = results[0].location.city
+    that.state = results[0].location.state
+    that.postcode = results[0].location.postcode
+    that.phone = results[0].phone
+    that.username = results[0].login.username
+    that.password = results[0].login.password
+}
+
+async function fetchDataAPI(that) {
+    const res = await fetch('https://randomuser.me/api')
+    const { results } = await res.json()
+
+    updateFields(that, results)
+}
 
 app.mount('#app')
